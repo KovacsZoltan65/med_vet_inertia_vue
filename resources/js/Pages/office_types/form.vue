7bf4865bf4c58a204895a1d13f46c7aaffe23b72
KovacsZoltan65 <49495656+KovacsZoltan65@users.noreply.github.com>
2023-05-01 18:52:24 +0200

    Office Type

--- resources/js/Pages/office_types/form.vue
+++ resources/js/Pages/office_types/form.vue
@@ -5,16 +5,12 @@
             
             <!-- NAME -->
             <div class="mb-4">
-                <label for="formName" 
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                >Name:</label>
-
-                <input type="text" v-model="form.name"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                    id="formName" placeholder="Enter Name" required>
-
-                <div v-if="$page.props.errors.name" class="text-red-500"
-                >{{ $page.props.errors.name }}</div>
+                <!--<label for="formName" class="block text-gray-700 text-sm font-bold mb-2">Name:</label>-->
+                <!--<input type="text" id="formName" v-model="form.name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Name" required>-->
+                <!--<div v-if="$page.props.errors.name" class="text-red-500">{{ $page.props.errors.name }}</div>-->
+                <InputLabel for="name" value="Name:" />
+                <TextInput id="name" name="name" v-model="form.name"/>
+                <InputError :message="$page.props.errors.name"/>
             </div>
 
         </div>
@@ -23,10 +19,17 @@
 </template>
 
 <script>
+import InputLabel from '../../MyComponents/InputLabel.vue';
+import TextInput from '../../MyComponents/TextInput.vue';
+import InputError from '../../MyComponents/InputError.vue';
 
 export default(await import('vue')).defineComponent({
     name: 'OfficeTypeForm',
     props: ['form'],
-    components: {}
+    components: { 
+        InputLabel, 
+        TextInput, 
+        InputError
+    }
 });
 </script>
