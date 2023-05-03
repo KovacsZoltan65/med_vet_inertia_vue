fc3c082abd9b1b8cfc83ada40143c6d8e70e47ac
KovacsZoltan65 <49495656+KovacsZoltan65@users.noreply.github.com>
2023-05-03 13:16:51 +0200

    Components

---
+++ resources/js/Components/buttons/DeleteButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <button :type="type"
+        class="text-white 
+            bg-red-700 
+            hover:bg-red-800 
+            focus:ring-4 
+            focus:outline-none 
+            focus:ring-red-300 
+            font-medium rounded-lg 
+            text-sm p-2.5 
+            text-center 
+            inline-flex 
+            items-center mr-2 
+            dark:bg-red-600 
+            dark:hover:bg-red-700 
+            dark:focus:ring-red-800">
+        <slot />
+    </button>
+</template>
+
+<script setup>
+    defineProps({
+        type: {
+            type: String,
+            default: 'submit'
+        }
+    });
+</script>
---
+++ resources/js/Components/buttons/EditButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button :type="type"
+        class="text-white 
+            bg-green-700 
+            hover:bg-green-800 
+            focus:ring-4 
+            focus:outline-none 
+            focus:ring-blue-300 
+            font-medium 
+            rounded-lg 
+            text-sm p-2.5 
+            text-center inline-flex items-center mr-2 
+            dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+        <slot />
+    </button>
+</template>
+
+<script setup>
+    defineProps({
+        type: {
+            type: String,
+            default: 'submit'
+        }
+    });
+</script>
