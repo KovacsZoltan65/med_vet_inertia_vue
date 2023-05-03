ec7950ed52e8448f4bd1146120d180c111d5a20a
KovacsZoltan65 <49495656+KovacsZoltan65@users.noreply.github.com>
2023-05-03 13:20:33 +0200

    Office Types

--- resources/js/Pages/office_types/index.vue
+++ resources/js/Pages/office_types/index.vue
@@ -1,83 +1,96 @@
 <script>
-import AppLayout from '../../Layouts/AppLayout.vue';
+    import AppLayout from '../../Layouts/AppLayout.vue';
 
-import Pagination from '../../Components/Pagination.vue';
-import DialogModal from '../../Components/DialogModal.vue';
-import OfficeTypeForm from './form.vue';
+    import Pagination from '../../Components/Pagination.vue';
+    import DialogModal from '../../Components/DialogModal.vue';
+    import TypeForm from './form.vue';
 
-import PrimaryButton from '../../Components/buttons/PrimaryButton.vue';
-import SecondaryButton from '../../Components/buttons/SecondaryButton.vue';
+    import PrimaryButton from '../../Components/buttons/PrimaryButton.vue';
+    import SecondaryButton from '../../Components/buttons/SecondaryButton.vue';
+    import AddButton from '../../Components/buttons/AddButton.vue';
+    import EditButton from '../../Components/buttons/EditButton.vue';
+    import DeleteButton from '../../Components/buttons/DeleteButton.vue';
 
-import ArrowRightOnRectangleIcon from '../../Components/icons/ArrowRightOnRectangleIcon.vue';
+    import ArrowRightOnRectangleIcon from '../../Components/icons/ArrowRightOnRectangleIcon.vue';
 
-import {
-    PlusIcon, PencilIcon, TrashIcon, CircleStackIcon
-} from '@heroicons/vue/24/solid';
+    import {
+        PlusIcon, PencilIcon, TrashIcon, CircleStackIcon
+    } from '@heroicons/vue/24/solid';
 
-import Success from '../../Components/alerts/Success.vue';
+    import Success from '../../Components/alerts/Success.vue';
 
-const defaultTypeObject = {
-    name: null
-};
+    const defaultTypeObject = {
+        name: null
+    };
 
-export default(await import('vue')).defineComponent({
-    name: 'OfficeTypes',
-    props: ['data'],
-    components: {
-        AppLayout,
-        Pagination,
-        DialogModal,
-        OfficeTypeForm,
-        PrimaryButton, SecondaryButton,
-        PlusIcon, PencilIcon, TrashIcon, CircleStackIcon,
-        Success, ArrowRightOnRectangleIcon
-    },
-    setup() {
-        return {
-            message: 'Hello Vue!'
-        }
-    },
-    data(){
-        return {
-            showModal: false,
-            isEdit: false,
-            formObject: defaultTypeObject,
-        }
-    },
-    mounted() {},
-    created() {},
-    methods: {
-        openForm(item) {
-            this.isEdit = !!item;
-            this.formObject = item ? Object.assign({}, item) : defaultTypeObject;
-            this.showModal = true;
-
-            this.$page.props.errors = {};
+    export default (await import('vue')).defineComponent({
+        name: 'Office Types',
+        props: ['data'],
+        components: {
+            AppLayout,
+            Pagination,
+            DialogModal,
+            TypeForm,
+            PrimaryButton, SecondaryButton, 
+            AddButton, EditButton, DeleteButton,
+            PlusIcon, PencilIcon, TrashIcon, CircleStackIcon,
+            Success, ArrowRightOnRectangleIcon
         },
-        closeForm() {
-            this.showModal = false;
-            this.formObject = defaultTypeObject;
-        },
-        saveItem(item){
-            let url = '/office_types';
-            if(item.id){
-                url += `/${item.id}`;
-                item._method = 'PUT';
+        setup() {},
+        data() {
+            return {
+                showModal: false,
+                isEdit: false,
+                formObject: defaultTypeObject,
+
+                selected: [],
+                selectAll: false,
             }
-
-            this.$inertia.post(url, item, {
-                onError: (err) => { console.log(err); },
-                onSuccess: () => {
-                    this.closeForm();
-                },
-            });
+        },
+        mounted() { },
+        created() { },
+        methods: {
             
-        },
-        deleteItem(){
-            console.log('deleteItem', this.formObject);
-        },
-    }
-});
+            select(){
+                this.selected = [];
+                if( !this.selectAll ){
+                    for( let i in this.data.data ){
+                        this.selected.push(this.data.data[i].id)
+                    }
+                }
+            },
+
+            openForm(item) {
+                this.isEdit = !!item;
+                this.formObject = item ? Object.assign({}, item) : defaultTypeObject;
+                this.showModal = true;
+
+                this.$page.props.errors = {};
+            },
+            closeForm() {
+                this.showModal = false;
+                this.formObject = defaultTypeObject;
+            },
+            saveItem(item) {
+                let url = '/office_types';
+                if (item.id) {
+                    url += `/${item.id}`;
+                    item._method = 'PUT';
+                }
+
+                this.$inertia.post(url, item, {
+                    onError: (err) => { console.log(err); },
+                    onSuccess: () => {
+                        this.closeForm();
+                    },
+                });
+
+            },
+            deleteItem() {
+                console.log('deleteItem', this.formObject);
+            },
+        }
+    });
 </script>
 
 <template>
@@ -93,82 +106,108 @@ export default(await import('vue')).defineComponent({
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
 
                     <!-- Üzenetek -->
-                    <Success :message="$page.props.flash.message" v-if="$page.props.flash.message"/>
+                    <Success :message="$page.props.flash.message" v-if="$page.props.flash.message" />
 
                     <!-- NEW button -->
-                    <PrimaryButton 
-                        type="button" 
-                        @click="openForm()">
+                    <AddButton type="button" @click="openForm()">
                         <PlusIcon class="h-5 w-5" />
-                    </PrimaryButton>
+                    </AddButton>
 
                     <!-- Book list -->
-                    <table class="table table-bordered table-fixed w-full posts-table">
-                        <thead>
-                            <tr class="bg-gray-100">
-                                <th class="px-4 py-2 w-20">No.</th>
-                                <th class="px-4 py-2">Name</th>
-                                <th class="px-4 py-2">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="item in data.data">
-                                <td class="px-4 py-2 border">{{ item.id }}</td>
-                                <td class="px-4 py-2 border">{{ item.name }}</td>
-                                <td class="px-4 py-2 border">
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        
+                        <div class="text-uppercase text-bold">id selected: {{selected}}</div>
 
-                                    <!-- EDIT button -->
-                                    <PrimaryButton type="button" 
-                                        @click="openForm(item)">
-                                        <PencilIcon class="h-5 w-5" />
-                                    </PrimaryButton>
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <!-- SELECT ALL -->
+                                    <th scope="col" class="p-4">
+                                        <div class="flex items-center">
+                                            <input id="checkbox-all-search" 
+                                                type="checkbox"
+                                                v-model="selectAll" 
+                                                @click="select"
+                                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                            <label for="checkbox-all-search" 
+                                                class="sr-only">checkbox</label>
+                                        </div>
+                                    </th>
+                                    <!-- NAME -->
+                                    <th scope="col" class="px-6 py-3">Name</th>
+                                    <!-- ACTION -->
+                                    <th scope="col" class="px-6 py-3">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="item in data.data" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    
+                                    <!-- Row checkbox -->
+                                    <td class="w-4 p-4">
+                                        <div class="flex items-center">
+                                            <input id="checkbox-table-search-1" 
+                                                type="checkbox"
+                                                :value="item.id"
+                                                v-model="selected"
+                                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                            <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                                        </div>
+                                    </td>
+                                    
+                                    <!-- Name -->
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ item.name }}
+                                    </th>
 
-                                    <!-- DELETE button -->
-                                    <SecondaryButton class="ml-3">
-                                        <TrashIcon class="h-5 w-5" />
-                                    </SecondaryButton>
+                                    <td class="px-6 py-4">
 
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                        <!-- EDIT button -->
+                                        <EditButton type="button" 
+                                            @click="openForm(item)">
+                                            <PencilIcon class="h-5 w-5" />
+                                        </EditButton>
+
+                                        <!-- DELETE button -->
+                                        <DeleteButton class="ml-3">
+                                            <TrashIcon class="h-5 w-5" />
+                                        </DeleteButton>
+
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
 
                     <!-- PAGINATION -->
-                    <Pagination :links="data.links"></Pagination>
+                    <div class="flex items-center justify-between pt-4" aria-label="Table navigation">
+                        <Pagination :links="data.links"></Pagination>
+                    </div>
 
                 </div>
             </div>
         </div>
 
-        <!-- Company Modal -->
+        <!-- Post Modal -->
         <DialogModal :show="showModal">
             <template #title>
-                Office Types
+                Post
             </template>
             <template #content>
-                <OfficeTypeForm  
-                    :form="formObject"
-                ></OfficeTypeForm>
+                <TypeForm :form="formObject"></TypeForm>
             </template>
             <template #footer>
                 <!-- Cancel Button -->
-                <SecondaryButton type="button"
-                    @click="closeForm()" 
-                    class="self-start">
+                <SecondaryButton type="button" @click="closeForm()" class="self-start">
                     <ArrowRightOnRectangleIcon class="w-6 h-6" />
                 </SecondaryButton>
 
                 <!-- Create Button -->
-                <PrimaryButton type="button" class="ml-3"
-                    v-show="!isEdit" 
-                    @click="saveItem(formObject)">
+                <PrimaryButton type="button" class="ml-3" v-show="!isEdit" @click="saveItem(formObject)">
                     <CircleStackIcon class="h-5 w-5" />
                 </PrimaryButton>
 
                 <!-- Update Button -->
-                <PrimaryButton type="button" class="ml-3"
-                    v-show="isEdit" 
-                    @click="saveItem(formObject)">
+                <PrimaryButton type="button" class="ml-3" v-show="isEdit" @click="saveItem(formObject)">
                     <CircleStackIcon class="h-5 w-5" />
                 </PrimaryButton>
             </template>
